e6980a7e3adb9d763e08f71174ccedee7cc3ed25
Aaron Putterman <abp6318@rit.edu>
2022-05-30 14:26:49 -0400

    Implemented sleep function properly so no excess games are fetched

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
 console.log("Running playground...");
 
-// prompt/inform user to regen key: https://developer.riotgames.com/
-// fetch top 5 players
-// foreach player
-	// display player name
-	// display lp
-	// from last 20 games
-		// top 10 active traits played & average placement with each trait
-		// average placement
-		// # of wins
-		// # of top 4s
-		// # of bot 4s
-
-/**
-
-Prompt/flag for top x # of users
-Prompt/flag for estimated start time (Epoch timestamp in seconds)
-Prompt/flag for estimated end time (Epoch timestamp in seconds)
-
-Need to consider clearing ./patch/ directory of contents, or maybe checking for existing folders
-
-foreach puuid/
-    continueLoop = true;
-    startCounter = 0;
-    while/ continueLoop == true/
-        Get game (puuid; start=startCounter; count=1)
-        fetch game
-        if/ game's patch == specified patch/
-            append full game data to (file=puuid) in (directory="./patch/date/)
-            set start to true
-        /else/
-            if/ start == true/
-                continueLoop = false;
-        /
-        startCounter += 1
-        sleep for 1.5 seconds
-    /
-/
-
-
- */
-
-
 const axios = require('axios').default;
 const prompt = require('prompt-sync')();
 const fs = require('fs');
@@ -49,7 +7,6 @@ const fs = require('fs');
 const numberOfPlayers = 1;
 const start = 0;
 const count = 5;
-let toOutfile = "";
 
 let key = "";
 
@@ -61,54 +18,77 @@ if(process.argv.length == 2){
 
 fs.truncate('./out.txt', 0, function(){console.log('Emptying out.txt content...')})
 
-axios.get('https://na1.api.riotgames.com/tft/league/v1/challenger?api_key='+key)
-	.then(function (response) {
-		let entries = response.data.entries;
-		entries = entries.sort((a, b) => parseFloat(b.leaguePoints) - parseFloat(a.leaguePoints));
-
-		for (let index = 0; index < numberOfPlayers; index++) {
-			axios.get("https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/"+entries[index].summonerName+"?api_key="+key)
-				.then(function (response) {
-					let puuid = response.data.puuid;
-					axios.get("https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/"+puuid+"/ids?start="+start+"&count="+count+"&api_key="+key)
-						.then(function (response) {
-							for (let i = 0; i < response.data.length; i++) {								
-								setTimeout(function(i){
-									console.log("Fetching matchId="+response.data[i]);
-									axios.get("https://americas.api.riotgames.com/tft/match/v1/matches/"+response.data[i]+"?api_key="+key)
+function mainFunc(){
+	axios.get('https://na1.api.riotgames.com/tft/league/v1/challenger?api_key='+key)
+		.then(function (response) {
+			let entries = response.data.entries;
+			entries = entries.sort((a, b) => parseFloat(b.leaguePoints) - parseFloat(a.leaguePoints));
+			for (let index = 0; index < numberOfPlayers; index++) {
+				axios.get("https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/"+entries[index].summonerName+"?api_key="+key)
+					.then(async function (response) {
+						let puuid = response.data.puuid;
+						let continueLoop = true;
+						let startCounter = 0;
+						let startPatchCollection = false;
+						for (let i = 0; continueLoop; i++) {
+							console.log("i: "+i + "; continueLoop: " + continueLoop);
+							
+							Promise.resolve(axios.get("https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + puuid + "/ids?start=" + startCounter + "&count=1&api_key=" + key)
+								.then(function (response) {
+									console.log("Fetching matchId=" + response.data[0]);
+									startCounter += 1;
+									return axios.get("https://americas.api.riotgames.com/tft/match/v1/matches/" + response.data[0] + "?api_key=" + key)
 										.then(function (response) {
-											let gameInfo = response.data.info.participants;
-											let res = gameInfo.filter(function(elem){ 
-												if(elem.puuid == puuid) return elem;
-											});
-											// fs.appendFile('./out.txt', JSON.stringify(response.data.info) + "\n", err => {
-											fs.appendFile('./out.txt', JSON.stringify(res) + "\n", err => {
-												if (err) {
-													console.error(err);
-												}
-											});
+											console.log("^--" + response.data.info.game_version);
+											if (response.data.info.game_version.endsWith("<Releases/12.9>")) {
+												startPatchCollection = true;
+												let gameInfo = response.data.info.participants;
+												let res = gameInfo.filter(function (elem) {
+													if (elem.puuid == puuid)
+														return elem;
+												});
+												fs.appendFile('./out.txt', JSON.stringify(res) + "\n", err => {
+													if (err) {
+														console.error(err);
+														return false;
+													}
+												});
+												return true;
+											} else {
+												return !(startPatchCollection == true);
+											}
 										})
 										.catch(function (error) {
+											console.log("ERROR: STATION 1");
 											console.log(error);
-										})
-								}, 1500 * i, i);
-							}
-						})
-						.catch(function (error) {
-							console.log(error);
-						})
-				})
-				.catch(function (error) {
-					console.log(error);
-				})
-		}
-	})
-	.catch(function (error) {
-		console.log(error);
-	});
+										});
+								})
+								.catch(function (error) {
+									console.log("ERROR: STATION 2");
+									console.log(error);
+								})
+							).then(function(value){ continueLoop = value; });
+							await sleep(1250);
+							if(i>275) break;
+						} // while loop
+					})
+					.catch(function (error) {
+						console.log("ERROR: STATION 3");
+						console.log(error);
+					})
+			}
+		})
+		.catch(function (error) {
+			console.log("ERROR: STATION 4");
+			console.log(error);
+		});
+}
 
-// fs.writeFile('./out.txt', toOutfile, err => {
-// 	if (err) {
-// 		console.error(err);
-// 	}
-// });
+mainFunc();
+
+
+function sleep(ms) {
+	return new Promise((resolve) => {
+	  setTimeout(resolve, ms);
+	});
+  }
